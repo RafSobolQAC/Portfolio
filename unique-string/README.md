cb41fa9d5764605e6d4de463adf89a53619148b9
Raf <rafal.sobol@academytrainee.com>
2020-02-19 09:31:27 +0000

    Added stream method & readme to unique-string.

---
+++ unique-string/README.md
@@ -0,0 +1,4 @@
+# Unique string
+
+This script takes in a string and returns a string composed only of distinct characters from the input string.
+
--- unique-string/src/com/qa/exercises/Runner.java
+++ unique-string/src/com/qa/exercises/Runner.java
@@ -1,20 +1,27 @@
 package com.qa.exercises;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Runner {
 	public static String uniqueString(String input) {
-		String ret = "";
-		boolean isUnique = true;
-		for (int i = 0; i < input.length(); i++) {
-			for (int j = 0; j < ret.length(); j++) {
-				if (input.substring(i,i+1).equals(ret.substring(j,j+1))) {
-					isUnique = false;
-					break;
-				}
-			}
-			if (isUnique) ret+=input.substring(i,i+1);
-			isUnique = true;
-		}
-		return ret;
+		
+		return Arrays.asList(input.split("")).stream().distinct().collect(Collectors.joining(""));
+		
+		
+//		String ret = "";
+//		boolean isUnique = true;
+//		for (int i = 0; i < input.length(); i++) {
+//			for (int j = 0; j < ret.length(); j++) {
+//				if (input.substring(i,i+1).equals(ret.substring(j,j+1))) {
+//					isUnique = false;
+//					break;
+//				}
+//			}
+//			if (isUnique) ret+=input.substring(i,i+1);
+//			isUnique = true;
+//		}
+//		return ret;
 		
 		
 		

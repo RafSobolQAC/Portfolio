ce7667b6b7e5e00e9cd5a1eab3df86fcf8280647
Raf <rafal.sobol@academytrainee.com>
2020-02-21 09:24:38 +0000

    Added recursive Fibonacci finder.

--- fibonacci/src/com/qa/exercises/Runner.java
+++ fibonacci/src/com/qa/exercises/Runner.java
@@ -15,8 +15,15 @@ public class Runner {
 		return curr;
 		
 	}
+	
+	public static int fibonacciRecursive(int num) {
+		if (num==0) return 0;
+		if (num==1) return 1;
+		return fibonacciRecursive(num-1)+fibonacciRecursive(num-2);
+	}
 	public static void main(String[] args) {
 		System.out.println(fibonacciFinder(10));
+		System.out.println(fibonacciRecursive(10));
 	}
 	
 }
